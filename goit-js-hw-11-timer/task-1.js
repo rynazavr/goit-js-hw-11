efde55fb3eb2d05ebfc91a4b175472eee958d21e
rynazavr <lunevaira@gmail.com>
2020-06-12 20:09:49 +0300

    timer up without constructor

---
+++ goit-js-hw-11-timer/task-1.js
@@ -0,0 +1,56 @@
+const refs = {
+  dataDays: document.querySelector('span[data-value="days"]'),
+  dataHours: document.querySelector('span[data-value="hours"]'),
+  dataMinutes: document.querySelector('span[data-value="mins"]'),
+  dataSeconds: document.querySelector('span[data-value="secs"]'),
+};
+const timer = {
+  start() {
+    const targetDate = new Date("Jul 17, 2019");
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - targetDate;
+      //   console.log(deltaTime);
+      updateTimer(deltaTime);
+    }, 1000);
+  },
+};
+timer.start();
+
+function updateTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const minutes = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const seconds = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.dataDays.textContent = days;
+  refs.dataHours.textContent = hours;
+  refs.dataMinutes.textContent = minutes;
+  refs.dataSeconds.textContent = seconds;
+}
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+/*
+class CountdownTimer {
+    constructor(
+        { selector, targetDate })
+    {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = targetDate - currentTime;
+    }, 1000);
+  }
+}
+
+
+ 
+new CountdownTimer({
+    selector: "#timer-1",
+    targetDate: new Date("Jul 17, 2019"),
+});
+*/
---
+++ goit-js-hw-11-timer/task2.js
@@ -0,0 +1,31 @@
+const parag = document.querySelector(".js_text");
+const logicPrint = {
+  arrText: ["Hello, Max. Please, check my homework =)"],
+  start() {
+    let counterLetters = 0;
+    let counterEl = 0;
+
+    setInterval(() => {
+      counterLetters += 1;
+
+      const lengthEl = this.arrText[counterEl].length;
+      const letters = this.arrText[counterEl].slice(0, counterLetters);
+
+      if (lengthEl === counterLetters) {
+        counterLetters = 0;
+
+        this.arrText.length === counterEl + 1
+          ? (counterEl = 0)
+          : (counterEl += 1);
+      }
+
+      this.printText(letters);
+    }, 100);
+  },
+
+  printText(letters) {
+    parag.textContent = letters;
+  },
+};
+
+logicPrint.start();
