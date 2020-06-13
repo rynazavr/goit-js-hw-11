2c112a1738333308a75afd0fde380b9fabaa6967
rynazavr <lunevaira@gmail.com>
2020-06-13 20:20:27 +0300

    no way

--- goit-js-hw-11-timer/task-1.js
+++ goit-js-hw-11-timer/task-1.js
@@ -1,10 +1,10 @@
 const refs = {
-  divBox: document.querySelector("#timer-1"),
   dataDays: document.querySelector('span[data-value="days"]'),
   dataHours: document.querySelector('span[data-value="hours"]'),
   dataMinutes: document.querySelector('span[data-value="mins"]'),
   dataSeconds: document.querySelector('span[data-value="secs"]'),
 };
+
 const timer = {
   start() {
     const targetDate = new Date("Jul 17, 2020");
@@ -12,7 +12,6 @@ const timer = {
       timerId = setInterval(() => {
         const currentTime = Date.now();
         const deltaTime = targetDate - currentTime;
-        //   console.log(deltaTime);
         updateTimer(deltaTime);
       }, 1000);
     } else {
@@ -39,24 +38,46 @@ function pad(value) {
   return String(value).padStart(2, "0");
 }
 
-/*
-class CountdownTimer {
-    constructor(
-        { selector, targetDate })
-    {
-    this.selector = selector;
-    this.targetDate = targetDate;
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = targetDate - currentTime;
-    }, 1000);
-  }
-}
+// class CountdownTimer {
+//   constructor({ selector, targetDate }) {
+//     this._selector = selector;
+//     this._targetDate = targetDate;
+//   }
+//   start() {
+//     if (Date.now() < this._targetDate) {
+//       timerId = setInterval(() => {
+//         const currentTime = Date.now();
+//         const deltaTime = this._targetDate - currentTime;
+//         updateTimer(deltaTime);
+//       }, 1000);
+//     } else {
+//       clearInterval(timerId);
+//     }
+//   }
+//   function updateTimer(time) {
+//     const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//     const hours = pad(
+//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//     );
+//     const minutes = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const seconds = pad(Math.floor((time % (1000 * 60)) / 1000));
+//     const timer1 = document.querySelector(`${this._selector}`);
+//     const dataDays = timer1.querySelector('[data-value="days"]');
+//     const dataHours = timer1.querySelector('[data-value="hours"]');
+//     const dataMinutes = timer1.querySelector('[data-value="mins"]');
+//     const dataSeconds = timer1.querySelector('[data-value="secs"]');
+//     dataDays.textContent = days;
+//     dataHours.textContent = hours;
+//     dataMinutes.textContent = minutes;
+//     dataSeconds.textContent = seconds;
+//   }
+//   function pad(value) {
+//     return String(value).padStart(2, "0");
+//   }
+// }
 
-
- 
-new CountdownTimer({
-    selector: "#timer-1",
-    targetDate: new Date("Jul 17, 2019"),
-});
-*/
+// const newTimer = new CountdownTimer({
+//   selector: "#timer-1",
+//   targetDate: new Date("Jul 17, 2020"),
+// });
+// newTimer.start();
