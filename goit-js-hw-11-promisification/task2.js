4352f9b491ef0a3c8cc9f3e5958f2f740d4de13d
rynazavr <lunevaira@gmail.com>
2020-06-15 18:33:38 +0300

    task3 with broken delay

--- goit-js-hw-11-promisification/task2.js
+++ goit-js-hw-11-promisification/task2.js
@@ -18,7 +18,3 @@ const logger = (updatedUsers) => console.table(updatedUsers);
 
 toggleUserState(users, "Mango").then(logger);
 toggleUserState(users, "Lux").then(logger);
-/*Перепиши функцию toggleUserState() так, 
-чтобы она не использовала callback-функцию callback, 
-а принимала всего два параметра allUsers и userName 
-и возвращала промис.*/
